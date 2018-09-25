28886da1031c45fb0ef0ecae18ab60836fec52d3
M1048350 <M1048350@A2ML21423.mindtree.com>
2018-09-25 17:10:47 +0530

    Up to Date with migration of data.

--- DTH/src/main/resources/db/migration/V1__Migration_Script.sql
+++ DTH/src/main/resources/db/migration/V1__Migration_Script.sql
@@ -17,6 +17,7 @@ CREATE DATABASE IF NOT EXISTS `dth` ;
 -- ----------------------------------------------------------------------------
 -- Table dth.channels
 -- ----------------------------------------------------------------------------
+USE `dth`;
 CREATE TABLE IF NOT EXISTS `dth`.`channels` (
   `CHANNEL_ID` INT(11) NOT NULL,
   `CHANNEL_NAME` VARCHAR(20) NULL DEFAULT NULL,
@@ -34,8 +35,7 @@ CREATE TABLE IF NOT EXISTS `dth`.`customer` (
   `REGISTERED_MOBILE` BIGINT(20) NULL DEFAULT NULL,
   `FIRST_NAME` VARCHAR(30) NULL DEFAULT NULL,
   `LAST_NAME` VARCHAR(30) NULL DEFAULT NULL,
-  `mobile` BIGINT(20) NOT NULL,
-  PRIMARY KEY (`SUBSCRIBER_ID`))
+   PRIMARY KEY (`SUBSCRIBER_ID`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
@@ -62,82 +62,3 @@ AUTO_INCREMENT = 23
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
--- ----------------------------------------------------------------------------
--- Routine dth.createChannels
--- ----------------------------------------------------------------------------
-DELIMITER $$
-
-DELIMITER $$
-USE `dth`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `createChannels`()
-BEGIN
-
-	DROP TABLE IF EXISTS CHANNELS;
-	create table CHANNELS(
-	CHANNEL_ID INT PRIMARY KEY,
-	CHANNEL_NAME VARCHAR(20),
-	COST_PER_MONTH DECIMAL
-	);
-
-END$$
-
-DELIMITER ;
-
--- ----------------------------------------------------------------------------
--- Routine dth.create_Cutomer
--- ----------------------------------------------------------------------------
-DELIMITER $$
-
-DELIMITER $$
-USE `dth`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `create_Cutomer`()
-BEGIN
-		DROP TABLE IF EXISTS CUSTOMER;
-
-		CREATE TABLE CUSTOMER (
-			SUBSCRIBER_ID BIGINT PRIMARY KEY,
-			REGISTERED_MOBILE BIGINT ,
-			FIRST_NAME VARCHAR(30), 
-			LAST_NAME VARCHAR(30)
-		);
-END$$
-
-DELIMITER ;
-
--- ----------------------------------------------------------------------------
--- Routine dth.insertChannels
--- ----------------------------------------------------------------------------
-DELIMITER $$
-
-DELIMITER $$
-USE `dth`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `insertChannels`()
-BEGIN
-INSERT INTO CHANNELS VALUES(101,'STAR PLUS',3.70);
-INSERT INTO CHANNELS VALUES(102,'DISCOVERY',4.00);
-INSERT INTO CHANNELS VALUES(103,'STAR MOVIES',5.50);
-INSERT INTO CHANNELS VALUES(104,'NDTV',13.00);
-INSERT INTO CHANNELS VALUES(105,'ZEE',12.0);
-INSERT INTO CHANNELS VALUES(106,'SPORTS',4.50);
-INSERT INTO CHANNELS VALUES(107,'TIMES NOW',6.50);
-
-END$$
-
-DELIMITER ;
-
--- ----------------------------------------------------------------------------
--- Routine dth.insertCustomers
--- ----------------------------------------------------------------------------
-DELIMITER $$
-
-DELIMITER $$
-USE `dth`$$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `insertCustomers`()
-BEGIN
-	INSERT INTO CUSTOMER VALUES (1102345678, 7284957435, 'SARBJOT','SINGH');
-	INSERT INTO CUSTOMER VALUES (1102345679, 7284957435, 'GURPREET','SINGH');
-	INSERT INTO CUSTOMER VALUES (1102345676, 7284957435, 'MINAL','KUMARI');
-END$$
-
-DELIMITER ;
-SET FOREIGN_KEY_CHECKS = 1;
