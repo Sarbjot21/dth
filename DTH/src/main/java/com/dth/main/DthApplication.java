666faab55f76b2308685b7e4f1129ab0701cf11e
M1048350 <M1048350@A2ML21423.mindtree.com>
2018-09-26 12:20:59 +0530

    remove extra dependencies

--- DTH/src/main/java/com/dth/main/DthApplication.java
+++ DTH/src/main/java/com/dth/main/DthApplication.java
@@ -1,9 +1,12 @@
 package com.dth.main;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class DthApplication {
 
 	public static void main(String[] args) {
