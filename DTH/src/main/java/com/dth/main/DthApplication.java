56842de66ccf5928228655e84a57c1499c0e9347
M1048350 <M1048350@A2ML21423.mindtree.com>
2018-09-27 10:59:22 +0530

    DTHApplication Update

--- DTH/src/main/java/com/dth/main/DthApplication.java
+++ DTH/src/main/java/com/dth/main/DthApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration()
 public class DthApplication {
 
 	public static void main(String[] args) {
