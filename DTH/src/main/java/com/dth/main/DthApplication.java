0fd46f2186ca72477df9d228e26e2eabca84cb20
M1048350 <M1048350@A2ML21423.mindtree.com>
2018-09-27 11:35:31 +0530

    Updated

--- DTH/src/main/java/com/dth/main/DthApplication.java
+++ DTH/src/main/java/com/dth/main/DthApplication.java
@@ -3,7 +3,7 @@ package com.dth.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 
 @SpringBootApplication
 @EnableAutoConfiguration()
